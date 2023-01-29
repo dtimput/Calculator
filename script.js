61548baa7239084329618fabf4f000c94bec30e6
Dylan <122069647+dtimput@users.noreply.github.com>
2023-01-29 15:18:39 -0800

    Added display logic

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
-const calculator = document.querySelector('.clear');
+const display = document.querySelector('.display');
+const buttons = document.querySelectorAll('.button');
+let onScreen = [0];
 
-calculator.addEventListener('click', click =>{
-    console.log('click');
-});
+function updateDisplay(e){
+    button = e.target.textContent;
+    if(onScreen[0] == 0 && !isNaN(button)){
+        onScreen[0] = button;
+        display.textContent = onScreen;
+    }else if((!isNaN(button) && onScreen.length <= 10) || (button == '.' && !onScreen.includes('.') && onScreen.length <= 10)){
+        onScreen.push(button);
+        display.textContent = onScreen.join('');
+    }else if(button == 'CLEAR'){
+        clearDisplay();
+    }else if(button == 'DELETE'){
+        deleteDisplay();
+    }
+}
+
+function clearDisplay(){    
+    onScreen = [0];
+    display.textContent = onScreen;
+}
+
+function deleteDisplay(){
+    if(onScreen.length > 1){
+        onScreen.pop();
+        display.textContent = onScreen.join('');
+    }else{
+        onScreen = [0];
+        display.textContent = onScreen;
+    }
+    
+}
+
+buttons.forEach(button => button.addEventListener('click', updateDisplay));
